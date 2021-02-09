61f0ce8aa514399149e974717180c38f7e078c87
Mateo Borja <66144847+Matt-NBR@users.noreply.github.com>
2021-02-08 21:40:29 -0500

    Add files via upload

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -30,16 +30,24 @@ const UserList = ({ users }) => {
     document.querySelector("#lastname").value = "";
   };
 
+  const handleDeleteUser = (event) => {
+    const deletedUser = usersList.pop();
+    console.log("Eliminar", deletedUser);
+    setUsersList((usersList) => {
+      return [...usersList];
+    });
+  };
+
   return (
     <>
       <h1>Lista de usuarios ({usersList.length} usuarios)</h1>
       <input type="text" id="name" placeholder="Ingrese un nombre" />
       <input type="text" id="lastname" placeholder="Ingrese un apellido" />
       <button onClick={handleAddUser}>Añadir</button>
-      <button>Eliminar el último usuario</button>
+      <button onClick={handleDeleteUser}>Eliminar el último usuario</button>
       <ul>
         {usersList.map((user, index) => {
-          return <li key={Math.random()}>{formatName(user)}</li>;
+          return <li key={index}>{formatName(user)}</li>;
         })}
       </ul>
     </>
